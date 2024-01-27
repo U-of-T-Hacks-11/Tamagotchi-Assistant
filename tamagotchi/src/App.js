4fa82db542750179720b49a5f327ec24886ce2fd
elaimizrahi <mizrahi.elai@gmail.com>
2024-01-27 00:42:17 -0500

    test

--- tamagotchi/src/App.js
+++ tamagotchi/src/App.js
@@ -1,26 +1,8 @@
-import './App.css';
-import Character from './Character';
 
-function App() {
-  return (
-    <div className="App">
-      <img src="/images/c1.png" alt="" />
-      <Character />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+const App = () => {
+  return <div>Some injected content</div>
 }
 
 export default App;
