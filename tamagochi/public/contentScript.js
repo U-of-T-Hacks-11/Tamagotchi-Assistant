a9ffafee72545fbdbc45962374b29409855c9e4e
Jingyue Wu <jingyue804@gmail.com>
2024-01-27 01:31:42 -0500

    setup chrome extension

--- tamagochi/public/contentScript.js
+++ tamagochi/public/contentScript.js
@@ -1,4 +1,4 @@
 // contentScript.js
-const div = document.createElement('div');
-div.innerText = 'Hello from your Chrome extension!';
-document.body.appendChild(div);
+// const div = document.createElement('div');
+// div.innerText = 'Hello from your Chrome extension!';
+// document.body.appendChild(div);
