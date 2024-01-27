d350560854f491ed8129c4931b2c53b76e868337
Richard Nguyen <richardng1311@gmail.com>
2024-01-27 16:20:30 -0500

    Deleted redundant text

--- tamagochi/src/App.js
+++ tamagochi/src/App.js
@@ -19,7 +19,6 @@ function App() {
         <Character currentHealthImageIndex={currentHealthImageIndex} setCurrentHealthImageIndex={setCurrentHealthImageIndex}/>
       </div>
     </main>
-    
   )
 }
 
