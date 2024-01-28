93f76ddb3b36f2f6f610af2f57f130fd0423c25e
elaimizrahi <mizrahi.elai@gmail.com>
2024-01-27 23:57:16 -0500

    notes

--- tamagochi/src/pages/Notes.js
+++ tamagochi/src/pages/Notes.js
@@ -4,19 +4,20 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import { FaSave } from "react-icons/fa";
 import { getAllNotes, createNote, deleteNote } from './mongoapi'; // Replace with the correct path
+import './Chat.css';
 
 const Notes = () => {
     const [notepad, setNotepad] = useState(false);
     const [notes, setNotes] = useState([]);
     const [description, setDescription] = useState('');
     const [title, setTitle] = useState('');
-    console.log(notes)
+    const [selectedNote, setSelectedNote] = useState(undefined);
+    const [noteCreated, setNoteCreated] = useState(false);
 
     useEffect(() => {
         const fetchNotes = async () => {
             try {
                 const fetchedNotes = await getAllNotes();
-                console.log(fetchedNotes)
                 setNotes(fetchedNotes);
             } catch (error) {
                 console.error('Error fetching notes:', error.message);
@@ -33,6 +34,7 @@ const Notes = () => {
             setDescription('');
             setTitle('');
             setNotepad(false);
+            setNoteCreated(true);
         } catch (error) {
             console.error('Error creating a new note:', error.message);
         }
@@ -59,27 +61,56 @@ const Notes = () => {
         <>
             {!notepad && (
                 <>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-                    <Button variant="outlined" style={{ marginTop: '10px' }} onClick={activateNotepad}>
-                        Add a new note!
-                    </Button>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                    <h2>Your Notes:</h2>
-                    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '16px', justifyContent: 'center' }}>
-                        {notes.map((note, index) => (
-                            <div key={index} style={{ border: '1px solid #ccc', padding: '8px', borderRadius: '4px', justifyContent: 'space-between', width: '50px' }}>
-                                <FaSave style={{ marginRight: '8px' }} />
-                                {note.title}
-                            </div>
-                        ))}
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
+                        <button className='chat-button' onClick={activateNotepad}>
+                            Add a new note!
+                        </button>
                     </div>
-                </div>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: '-30px' }}>
+                        {!selectedNote && (
+                            <div style={{ marginTop: '10px' }}>
+                                <h2>Your Notes:</h2>
+                                <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '16px', justifyContent: 'center', overflow: 'auto', height: '260px' }}>
+                                    {notes.map((note, index) => (
+                                        <div onClick={() => setSelectedNote(index + 1)} key={index} style={{ border: '1px solid #ccc', padding: '8px', borderRadius: '4px', justifyContent: 'space-between', width: '50px' }}>
+                                            <FaSave style={{ marginRight: '8px' }} />
+                                            {note.title}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                        {selectedNote && (
+                            <div style={{ position: 'relative', marginTop: '100px' }}>
+                                <button
+                                    onClick={() => setSelectedNote(undefined)}
+                                    style={{
+                                        position: 'absolute',
+                                        top: 0,
+                                        left: -50,
+                                        backgroundColor: 'red',
+                                        color: 'white',
+                                        border: 'none',
+                                        padding: '5px',
+                                        cursor: 'pointer',
+                                    }}
+                                >
+                                    X
+                                </button>
+                                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: '20px' }}>
+                                    <Stack>
+                                        <h2>{notes[selectedNote - 1].title}</h2>
+                                        <div>{notes[selectedNote - 1].description}</div>
+                                    </Stack>
+                                </div>
+                            </div>
+                        )}
+                    </div>
                 </>
             )}
             {notepad && (
                 <>
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '20vh' }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: noteCreated ? '60vh' : '40vh', marginTop: '20px' }}>
                         <Stack alignItems={'center'}>
                             <TextField
                                 id="standard-textarea"
@@ -101,12 +132,13 @@ const Notes = () => {
                                 value={description}
                                 onChange={(e) => setDescription(e.target.value)} // Update the description state
                             />
-                            <Button variant="outlined" style={{ marginTop: '10px' }} onClick={() => handleCreateNote(title, description)}>
+                            <Button className='chat-button' onClick={() => { handleCreateNote(title, description); setNoteCreated(true) }}>
                                 Save your note!
                             </Button>
-                            <Button variant="outlined" style={{ marginTop: '10px' }} onClick={() => setNotepad(false)}>
-                                return
+                            <Button className='chat-button' onClick={() => { setNotepad(false); setNoteCreated(false) }}>
+                                Return
                             </Button>
+                            <div>{noteCreated && <div className='chat-button'>Note Created!</div>}</div>
                         </Stack>
                     </div>
                 </>
