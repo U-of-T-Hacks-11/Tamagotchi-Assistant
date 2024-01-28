a6b1eaf2da0adf7f0158f7d51d3fd0a7058ed5ee
Richard Nguyen <richardng1311@gmail.com>
2024-01-27 21:40:31 -0500

    cleaned up comments

--- tamagochi/src/pages/Character.js
+++ tamagochi/src/pages/Character.js
@@ -27,27 +27,22 @@ export const Character = ({ currentHealthImageIndex, setCurrentHealthImageIndex 
         const minPosition = 0;
         const maxPosition = screenWidth;
 
-        // Check if the health bar has reached index 4
         if (currentHealthImageIndex === 4) {
             setIsStopped(true);
-            return; // Stop moving
+            return;
         }
 
         const intervalId = setInterval(() => {
-            // Check if the character has reached the right or left edge of the screen
             if (direction === 1 && position + characterWidth > maxPosition) {
-                // Move to the left edge and change direction to -1
                 setPosition(maxPosition - characterWidth);
                 setDirection(-1);
             } else if (direction === -1 && position < minPosition) {
-                // Move to the right edge and change direction to 1
                 setPosition(minPosition);
                 setDirection(1);
             } else {
-                // Update the position based on the current direction
                 setPosition((prevPosition) => (prevPosition + direction*0.5));
             }
-        }, 800); // Increase the interval delay for slower movement
+        }, 800);
 
         const handleResize = () => {
             setScreenWidth(window.innerWidth);
