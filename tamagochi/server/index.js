f6406bf33ab117d5a50dbb2230669e92587a1263
Jingyue Wu <jingyue804@gmail.com>
2024-01-28 04:39:18 -0500

    Finish

--- tamagochi/server/index.js
+++ tamagochi/server/index.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const { CohereClient } = require('cohere-ai')
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mizrahielai:bUaFocH6zmq3QLFE@tamagotchi.sqyejfv.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri =
+  'mongodb+srv://mizrahielai:bUaFocH6zmq3QLFE@tamagotchi.sqyejfv.mongodb.net/?retryWrites=true&w=majority'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -11,22 +12,24 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-});
+  },
+})
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await client.connect()
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await client.close()
   }
 }
-run().catch(console.dir);
+run().catch(console.dir)
 
 const app = express()
 var cors = require('cors')
@@ -53,42 +56,81 @@ const examples = [
   { text: 'Draft ideas for the project', label: 'Take notes' },
   { text: 'Create a checklist', label: 'Take notes' },
   { text: 'Write down important details', label: 'Take notes' },
+
+  // Summarizer
+  { text: 'Summarize the main points of the article', label: 'Summarizer' },
+  { text: 'Give me a concise overview of the report', label: 'Summarizer' },
+  {
+    text: 'Condense the key information from the document',
+    label: 'Summarizer',
+  },
+  { text: 'Provide a summary of the research findings', label: 'Summarizer' },
+  { text: 'Outline the main arguments in the essay', label: 'Summarizer' },
+  { text: 'Give me a brief recap of the key takeaways', label: 'Summarizer' },
+
+  // Take Notes
+  { text: 'Document key insights from the meeting', label: 'Take notes' },
+  {
+    text: 'Record important details from the conversation',
+    label: 'Take notes',
+  },
+  { text: 'Jot down critical points during the lecture', label: 'Take notes' },
+  {
+    text: 'Capture essential information for later reference',
+    label: 'Take notes',
+  },
+  {
+    text: 'Create a detailed record of the client discussion',
+    label: 'Take notes',
+  },
+  { text: 'Note down action items from the team meeting', label: 'Take notes' },
+  { text: 'Document the project requirements in a note', label: 'Take notes' },
+
+  // Timer
+  { text: 'Set a 45-minute focus timer', label: 'timer' },
+  { text: 'Begin a 20-minute productivity session', label: 'timer' },
+  { text: 'Initiate a 15-minute brainstorming timer', label: 'timer' },
+  { text: 'Start a 5-minute quick task timer', label: 'timer' },
+  { text: 'Set a timer for a 1-hour deep work session', label: 'timer' },
+
+  // Feed Pet
+  { text: 'Feed the cat its breakfast', label: 'Feed pet' },
+  { text: 'Give the dog a treat for good behavior', label: 'Feed pet' },
+  { text: 'Provide food and water for the pet rabbit', label: 'Feed pet' },
+  { text: 'Ensure the fish in the tank are fed', label: 'Feed pet' },
+  { text: 'Offer a snack to the parrot in the cage', label: 'Feed pet' },
+  { text: 'Give the hamster fresh food and water', label: 'Feed pet' },
 ]
 app.get('/api/router', async (req, res) => {
   try {
-    const message = req.headers['message'] + ' keep your response under 15 words' ||
-    'Default message if header is not provided'
+    const message =
+      req.headers['message'] + ' keep your response under 15 words' ||
+      'Default message if header is not provided'
 
-    
     const response = await cohere.classify({
       inputs: [message],
       examples: examples,
-    });
+    })
 
-    console.log(response.classifications[0].prediction, response.classifications[0].confidence);
-
-    // if (response.classifications[0].confidence > 0.7) {
-    //   res.json(response.classifications[0].prediction);
-    // } else {
-    //   res.json('Fallback');
-    // }
+    console.log(
+      response.classifications[0].prediction,
+      response.classifications[0].confidence
+    )
 
     if (response.classifications[0].confidence > 0.85) {
       res.json({
-        prediction: response.classifications[0].prediction
+        prediction: response.classifications[0].prediction,
         // confidence: response.classifications[0].confidence,
-      });
+      })
     } else {
       // res.json({ prediction: 'Fallback', confidence: 0 });
-      res.json({ prediction: 'Fallback'});
-
+      res.json({ prediction: 'Fallback' })
     }
-    
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(error)
+    res.status(500).json({ error: 'Internal Server Error' })
   }
-});
+})
 
 app.get('/api/chat', async (req, res) => {
   try {
@@ -132,68 +174,116 @@ app.get('/api/chat', async (req, res) => {
   }
 })
 
+// app.get('/api/summarize', async (req, res) => {
+//   console.log('summary!!')
+//   try {
+//     const userMessage = req.headers['message']
+//     const summarize = await cohere.summarize({
+//       text: userMessage,
+//     })
+
+//     console.log(summarize)
+
+//     // Assuming the 'summarize' response contains the summarized text in 'summary' property
+//     const summarizedText = summarize.summary || 'No summary available'
+
+//     res.json({ summary: summarizedText })
+//   } catch (error) {
+//     console.error('Error in /api/summarize:', error)
+//     res.status(500).json({ error: 'Internal Server Error' })
+//   }
+// })
+
+app.get('/api/summarize', async (req, res) => {
+  console.log('summary!!')
+
+  try {
+    const userMessage = req.headers['message']
+    console.log('User Message:', userMessage) // Add this line
+
+    const summarize = await cohere.summarize({
+      text: userMessage,
+    })
+
+    console.log('Summarize Response:', summarize) // Add this line
+
+    if (!summarize || summarize.error) {
+      console.error(
+        'Error in summarize API call:',
+        summarize?.error || 'Unknown error'
+      )
+      res.status(500).json({ error: 'Internal Server Error' })
+      return
+    }
+
+    const summarizedText = summarize.summary || 'No summary available'
+    res.json({ summary: summarizedText })
+  } catch (error) {
+    console.error('Error in /api/summarize:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`)
 })
 
-
 app.post('/api/notes', async (req, res) => {
-    try {
-        console.log(req.headers)
-        const title = req.headers['title']
-        const description = req.headers['description']
-      await client.connect();
-      const db = client.db('tamagotchi'); // Specify your database name
-      const notesCollection = db.collection('notes'); // Specify your collection name
-  
-      // Insert the new note into the database
-      const result = await notesCollection.insertOne({ title, description });
-  
-      // Respond with the inserted note
-    } catch (error) {
-      console.error('Error creating a new note:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
-  // Get all notes
-  app.get('/api/notes', async (req, res) => {
-    try {
-    await client.connect();
-      const db = client.db('tamagotchi'); // Specify your database name
-      const notesCollection = db.collection('notes'); // Specify your collection name
-  
-      // Fetch all notes from the database
-      const notes = await notesCollection.find().toArray();
-      console.log(notes);
-  
-      // Respond with the fetched notes
-      res.json(notes);
-    } catch (error) {
-      console.error('Error fetching notes:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
-  // Delete a note
-  app.delete('/api/notes/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const db = client.db('tamagotchi'); // Specify your database name
-      const notesCollection = db.collection('notes'); // Specify your collection name
-  
-      // Delete the note from the database
-      const result = await notesCollection.deleteOne({ _id: new ObjectId(id) });
-  
-      // Respond with the deletion status
-      if (result.deletedCount > 0) {
-        res.json({ message: 'Note deleted successfully' });
-      } else {
-        res.status(404).json({ message: 'Note not found' });
-      }
-    } catch (error) {
-      console.error('Error deleting a note:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  try {
+    console.log(req.headers)
+    const title = req.headers['title']
+    const description = req.headers['description']
+    await client.connect()
+    const db = client.db('tamagotchi') // Specify your database name
+    const notesCollection = db.collection('notes') // Specify your collection name
 
+    // Insert the new note into the database
+    const result = await notesCollection.insertOne({ title, description })
+
+    // Respond with the inserted note
+  } catch (error) {
+    console.error('Error creating a new note:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+// Get all notes
+app.get('/api/notes', async (req, res) => {
+  try {
+    await client.connect()
+    const db = client.db('tamagotchi') // Specify your database name
+    const notesCollection = db.collection('notes') // Specify your collection name
+
+    // Fetch all notes from the database
+    const notes = await notesCollection.find().toArray()
+    console.log(notes)
+
+    // Respond with the fetched notes
+    res.json(notes)
+  } catch (error) {
+    console.error('Error fetching notes:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+// Delete a note
+app.delete('/api/notes/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const db = client.db('tamagotchi') // Specify your database name
+    const notesCollection = db.collection('notes') // Specify your collection name
+
+    // Delete the note from the database
+    const result = await notesCollection.deleteOne({ _id: new ObjectId(id) })
+
+    // Respond with the deletion status
+    if (result.deletedCount > 0) {
+      res.json({ message: 'Note deleted successfully' })
+    } else {
+      res.status(404).json({ message: 'Note not found' })
+    }
+  } catch (error) {
+    console.error('Error deleting a note:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
--- tamagochi/src/components/Switcher.js
+++ tamagochi/src/components/Switcher.js
@@ -1,125 +1,136 @@
 import React, { useState } from 'react'
-import Chat from '../pages/Chat'
-import Home from '../pages/Home'
-import Timer from '../pages/Timer'
-import Feed from '../pages/Feed'
-import hungerBar from '../pages/images/hungerBar.png'
 import { FaHome, FaHourglassHalf, FaSave } from 'react-icons/fa'
 import { BsChatLeftTextFill } from 'react-icons/bs'
 import Notes from '../pages/Notes'
-import './Switcher.css'
+import Timer from '../pages/Timer'
+import Chat from '../pages/Chat'
+import Home from '../pages/Home'
 import Summarizer from '../pages/Summarizer'
+import { FaBookOpen } from "react-icons/fa";
+import hungerBar from '../pages/images/hungerBar.png'
+import './Switcher.css'
 
+const Switcher = ({ feedCharacter, charSelect, setCharacter }) => {
+  const [pageNumber, setPageNumber] = useState(0)
+  const handleRouterChange = (newRouter) => {
+    switch (newRouter) {
+      case 'timer':
+        setPageNumber(1)
+        break
+      case 'Take notes':
+        setPageNumber(3)
+        break
+      case 'Summarizer':
+        setPageNumber(4)
+        break
+      case 'Feed pet':
+        setPageNumber(0)
+        break
+      default:
+        break
+    }
+  }
+  const handleButtonClick = (component) => {
+    switch (component) {
+      case 'home':
+        setPageNumber(0)
+        break
+      case 'timer':
+        setPageNumber(1)
+        break
+      case 'chat':
+        setPageNumber(2)
+        break
+      case 'feed':
+        feedCharacter()
+        break
+      case 'notes':
+        setPageNumber(3)
+        break
+      case 'summarizer':
+        setPageNumber(4)
+        break
+      default:
+        setPageNumber(0)
+    }
+  }
 
-const Switcher = ({ feedCharacter }) => {
-  const [currentComponent, setCurrentComponent] = useState(<Home />)
-
-  const Switcher = ({ feedCharacter, charSelect, setCharacter }) => {
-    const [pageNumber, setPageNumber] = useState(0);
-    const handleRouterChange = (newRouter) => {
-      switch (newRouter) {
-        case 'timer':
-          setPageNumber(1);
-          break;
-        case 'chat':
-          // Pass showRes prop here
-          setPageNumber(2);
-          break;
-        // ... other cases
-
-        default:
-          break
-      }
-    };
-    const handleButtonClick = (component) => {
-      switch (component) {
-        case 'home':
-          setPageNumber(0);
-          break;
-        case 'timer':
-          setPageNumber(1);
-          break;
-        case 'chat':
-          setPageNumber(2);
-          break;
-        case 'feed':
-          feedCharacter()
-          break
-        case 'notes':
-          setPageNumber(3);
-          break;
-        default:
-          setPageNumber(0);
-      }
-    };
-
-    const renderPage = () => {
-      switch (pageNumber) {
-        case 0:
-          return <Home charSelect={charSelect} setCharacter={setCharacter} />;
-        case 1:
-          return <Timer />;
-        case 2:
-          return <Chat onRouterChange={handleRouterChange} />;
-        case 3:
-          return <Notes />;
-        default:
-          return null;
-      }
-    };
-
-    return (
-      <div className="App">
-        <div>
-          <div className="center-buttons">
-            <button className="button-icon" onClick={() => handleButtonClick('home')}>
-              <FaHome />
-            </button>
-            <button className="button-icon" onClick={() => handleButtonClick('timer')}>
-              <FaHourglassHalf />
-            </button>
-            <button className="button-icon" onClick={() => handleButtonClick('chat')}>
-              <BsChatLeftTextFill />
-            </button>
-            <button className="button-icon" onClick={() => handleButtonClick('chat')}>
-              <BsChatLeftTextFill />
-            </button>
-            <button className="button-icon" onClick={() => handleButtonClick('notes')}>
-              <FaSave />
-            </button>
-          </div>
-          <button
-            onClick={() => handleButtonClick('feed')}
-            style={{
-              position: 'absolute',
-              bottom: '30px',
-              right: '30px',
-              fontSize: '18px',
-              padding: '8px',
-              backgroundColor: 'rgba(0, 0, 0, 0.0)',
-              borderRadius: 10,
-              borderColor: 'rgba(14, 51, 19, 0.74)',
-              borderWidth: '4px',
-            }}
-          >
-            <img
-              src={hungerBar}
-              alt="Hunger Bar"
-              style={{ width: '50px', height: '50px' }}
-            />
-          </button>
-        </div>
-        <div>{renderPage()}</div>
-      </div>
-    );
-  };
+  const renderPage = () => {
+    switch (pageNumber) {
+      case 0:
+        return <Home charSelect={charSelect} setCharacter={setCharacter} />
+      case 1:
+        return <Timer />
+      case 2:
+        return <Chat onRouterChange={handleRouterChange} />
+      case 3:
+        return <Notes />
+      case 4:
+        return <Summarizer />
+      default:
+        return null
+    }
+  }
 
   return (
-    <div>
-      <Switcher />
+    <div className='App'>
+      <div>
+        <div className='center-buttons'>
+          <button
+            className='button-icon'
+            onClick={() => handleButtonClick('home')}
+          >
+            <FaHome />
+          </button>
+          <button
+            className='button-icon'
+            onClick={() => handleButtonClick('timer')}
+          >
+            <FaHourglassHalf />
+          </button>
+          <button
+            className='button-icon'
+            onClick={() => handleButtonClick('chat')}
+          >
+            <BsChatLeftTextFill />
+          </button>
+          <button
+            className='button-icon'
+            onClick={() => handleButtonClick('summarizer')}
+          >
+            <FaBookOpen />
+          </button>
+          <button
+            className='button-icon'
+            onClick={() => handleButtonClick('notes')}
+          >
+            <FaSave />
+          </button>
+        </div>
+        <button
+          onClick={() => handleButtonClick('feed')}
+          style={{
+            position: 'absolute',
+            bottom: '30px',
+            right: '30px',
+            fontSize: '18px',
+            padding: '8px',
+            backgroundColor: 'rgba(0, 0, 0, 0.0)',
+            borderRadius: 10,
+            borderColor: 'rgba(14, 51, 19, 0.74)',
+            borderWidth: '4px',
+          }}
+        >
+          <img
+            src={hungerBar}
+            alt='Hunger Bar'
+            style={{ width: '50px', height: '50px' }}
+          />
+        </button>
+      </div>
+      <div>{renderPage()}</div>
     </div>
-  );
-};
+  )
+}
 
-export default Switcher;
-
+export default Switcher
